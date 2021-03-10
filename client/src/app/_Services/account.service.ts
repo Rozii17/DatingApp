d3bb714c0893ca955ef7b1986264059f07397b76
Rozii17 <rozinaenator@gmail.com>
2021-03-10 12:31:10 +0100

    End of section 11

--- client/src/app/_Services/account.service.ts
+++ client/src/app/_Services/account.service.ts
@@ -22,8 +22,7 @@ export class AccountService {
         const user = response;
         if (user)
         {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
       })
     )
@@ -32,9 +31,9 @@ export class AccountService {
   register(model: any){
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
-        if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+        if (user) 
+        {
+          this.setCurrentUser(user);
         }
       })
     )
@@ -42,6 +41,7 @@ export class AccountService {
 
   setCurrentUser(user: User)
   {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
